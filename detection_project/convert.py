10ba4cfc21cf8698829726e0cdbd6745c7cb9858
BuildTools <unconfigured@null.spigotmc.org>
2022-08-05 14:01:52 +0300

    removed

--- detection_project/convert.py
+++
@@ -1,8 +0,0 @@
-import torch
-from models.crnn import CRNN
-model = CRNN(32, 1, 37, 256)
-model.load_state_dict(torch.load(
-    'CRNN-BaiduCTC.pth', map_location='cpu'))
-dummy_input = torch.randn(1, 1, 32, 100)
-torch.onnx.export(model, dummy_input,
-                  "CRNN-BaiduCTC.onnx", verbose=True)
